96692bae0efb9801ff9ebc903725d8271687fc0a
jess-linford <jess.linford@gmail.com>
2024-07-17 16:46:23 -0600

    upload frag_env.yaml

--- scripts/size_filter_bam.sh
+++ scripts/size_filter_bam.sh
@@ -5,7 +5,7 @@ threads=$2
 max_length=$3
 output=$4
 
-# Filter BAM file for reads <= 500bp and write to output file
+# Filter BAM file for reads <= max_length and write to output file
 samtools view -@ $threads -e "length(seq) <= $max_length" -O BAM -o $output $input
 
 # Index the final output
